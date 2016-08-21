aca714db5ecc5d46e36491e715802ad877a1533d
Joel Soderstrom <joel.soderstrom@gmail.com>
2016-08-22 00:02:50 +0200

    Code cleaning

--- api-gateway.js
+++ api-gateway.js
@@ -87,20 +87,7 @@ function createMessage(req) {
   };
 }
 
-function mergeResponses(httpResp, busResponse) {
-  httpResp.status = busResponse.status;
-}
-
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   var err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
-
-// error handlers  
-app.use(function(err, req, res, next) {
-  
+app.use(function(err, req, res, next) {  
   res.status(err.status || 500);
   
   var json = {
