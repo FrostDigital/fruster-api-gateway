5387563bccbf5f37534e6d7d431630fbbb33c5fc
Joel Söderström <joel.soderstrom@gmail.com>
2017-05-26 13:36:06 +0200

    Feature: Interceptors (#29)
    * Implements request interceptors
    
    * Makes sure to only invoke request interceptors
    
    * Implements interceptAction respond

--- api-gateway.js
+++ api-gateway.js
@@ -14,13 +14,18 @@ const utils = require("./utils");
 const uuid = require("uuid");
 const bearerToken = require("express-bearer-token");
 const request = require("request");
+const Minimatch = require("minimatch").Minimatch;
+const Promise = require("bluebird");
+const util = require("util");
 
 const reqIdHeader = "X-Fruster-Req-Id";
-
 const app = express();
 const dateStarted = new Date();
 
-var util = require("util");
+const interceptAction = {
+    respond: "respond",
+    next: "next"
+};
 
 app.use(cors({
     origin: conf.allowOrigin,
@@ -56,7 +61,7 @@ app.use(function (httpReq, httpRes, next) {
 
     logRequest(reqId, httpReq);
 
-    decodeToken(httpReq, reqId)
+    decodeToken(httpReq, reqId)            
         .then(decodedToken => sendInternalRequest(httpReq, reqId, decodedToken))
         .then(internalRes => sendHttpReponse(reqId, internalRes, httpRes))
         .catch(err => handleError(err, httpRes, reqId, reqStartTime));
@@ -65,7 +70,7 @@ app.use(function (httpReq, httpRes, next) {
 app.use(function (err, req, res, next) {
     res.status(err.status || 500);
 
-    var json = {
+    let json = {
         message: err.message
     };
 
@@ -106,22 +111,22 @@ function handleError(err, httpRes, reqId, reqStartTime) {
  * Token comes either in cookie or in header Authorization: Bearer <token>
  */
 function decodeToken(httpReq, reqId) {
-    var encodedToken = getToken(httpReq);
+    const encodedToken = getToken(httpReq);
 
     if (encodedToken) {
-        var decodeReq = {
+        const decodeReq = {
             reqId: reqId,
             data: encodedToken
         };
 
         return bus
-            .request('auth-service.decode-token', decodeReq)
+            .request("auth-service.decode-token", decodeReq)
             .then(resp => resp.data)
             .catch(err => {
                 if (err.status == 401 || err.status == 403) {
-                    log.debug('Failed to decode token (got error ' + err.code + ') will expire cookie if present');
+                    log.debug("Failed to decode token (got error " + err.code + ") will expire cookie if present");
                     err.headers = err.headers ||  {};
-                    err.headers['Set-Cookie'] = 'jwt=deleted; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT';
+                    err.headers["Set-Cookie"] = "jwt=deleted; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT";
                 }
                 throw err;
             });
@@ -130,9 +135,21 @@ function decodeToken(httpReq, reqId) {
     return Promise.resolve({});
 }
 
+function invokeRequestInterceptors(subject, message) { 
+    const matchedInterceptors = conf.interceptors.filter(interceptor => {
+        return interceptor.type === "request" && interceptor.match(subject);
+    });    
+    
+    return Promise.reduce(matchedInterceptors, (_message, interceptor) => {
+        if(_message.interceptAction === interceptAction.respond) {
+            return _message;
+        }
+        return bus.request(interceptor.targetSubject, _message);            
+    }, message);
+}
 
 function getToken(httpReq) {
-    var token;
+    let token;
 
     if (httpReq.token) {
         token = httpReq.token;
@@ -144,27 +161,36 @@ function getToken(httpReq) {
 }
 
 function sendInternalRequest(httpReq, reqId, decodedToken) {
-    let subject = utils.createSubject(httpReq);
-    let message = utils.createRequest(httpReq, reqId, decodedToken);
+    const subject = utils.createSubject(httpReq);
+    const message = utils.createRequest(httpReq, reqId, decodedToken);
+    
+    return invokeRequestInterceptors(subject, message)
+        .then(interceptedReq => {
 
-    log.debug('Sending to subject', subject);
-    log.silly(message);
+            if(interceptedReq.interceptAction === interceptAction.respond) {
+                delete interceptedReq.interceptAction;
+                return interceptedReq;
+            } 
 
-    // Multipart requests are dealt with manually so that
-    // api gateway is able to stream the multipart body to
-    // its internal receiving service.
+            log.debug("Sending to subject", subject);
+            log.silly(interceptedReq);
 
-    // Otherwise plain bus request is used, but note that 
-    // depending on what the recieving service wants for protocol
-    // the resulting request may still be done via HTTP. However, this
-    // happens under the hood in fruster-bus-js and hence is transparent for
-    // the api gateway.
+            // Multipart requests are dealt with manually so that
+            // api gateway is able to stream the multipart body to
+            // its internal receiving service.
 
-    if (isMultipart(httpReq)) {
-        return sendInternalMultipartRequest(subject, message, httpReq);
-    } else {
-        return sendInternalBusRequest(subject, message);
-    }
+            // Otherwise plain bus request is used, but note that 
+            // depending on what the recieving service wants for protocol
+            // the resulting request may still be done via HTTP. However, this
+            // happens under the hood in fruster-bus-js and hence is transparent for
+            // the api gateway.
+
+            if (isMultipart(httpReq)) {
+                return sendInternalMultipartRequest(subject, interceptedReq, httpReq);
+            } else {
+                return sendInternalBusRequest(subject, interceptedReq);
+            }
+        });
 }
 
 function sendInternalMultipartRequest(subject, message, httpReq) {
@@ -215,7 +241,7 @@ function sendHttpReponse(reqId, internalRes, httpRes) {
 
 function setRequestId(reqId, resp) {
     if (resp.reqId != reqId) {
-        log.warn('Request id in bus response (' + resp.reqId + ') does not match the one set by API gateway (' + reqId + ')');
+        log.warn(`Request id in bus response (${resp.reqId}) does not match the one set by API gateway (${reqId})`);
         resp.reqId = reqId;
     }
 }
@@ -262,19 +288,40 @@ function isMultipart(httpReq) {
     return httpReq.headers["content-type"] && httpReq.headers["content-type"].includes("multipart");
 }
 
+// function initInterceptors() {    
+//     interceptors = conf.interceptors.map(interceptor => {
+//         let split = interceptor.split(":"); 
+//         const patternSplit = split[0].split(",");
+//         const targetSubject = split[1];
+
+//         return {
+//             pattern: split[0],
+//             targetSubject: targetSubject,
+//             matchers: patternSplit.map(pattern => new Minimatch(pattern))
+//         };
+//     });
+
+//     log.info(`Initialized ${interceptors.length} interceptor(s)`); 
+// }
+
 module.exports = {
     start: function (httpServerPort, busAddress) {
 
-        var startHttpServer = new Promise(function (resolve, reject) {
+        let startHttpServer = new Promise(function (resolve, reject) {
             let server = http.createServer(app)
                 .listen(httpServerPort);
-            server.on('error', reject);
-            server.on('listening', resolve);
+            
+            server.on("error", reject);
+            
+            server.on("listening", () => {
+                log.info("HTTP server listening for on port", httpServerPort);     
+                resolve();
+            });
 
             return resolve(server);
         });
 
-        var connectToBus = function () {
+        let connectToBus = function () {
             return bus.connect(busAddress);
         };
 
---
+++ spec/interceptor.spec.js
@@ -0,0 +1,125 @@
+const request = require("request");
+const bus = require("fruster-bus");
+const conf = require("../conf");
+const apiGw = require("../api-gateway");
+const testUtils = require("fruster-test-utils");
+const interceptorConfig = require("../lib/interceptor-config");
+
+
+describe("Interceptors", function () {    
+
+    const httpPort = Math.floor(Math.random() * 6000 + 2000);
+    const baseUri = "http://127.0.0.1:" + httpPort;
+
+    testUtils.startBeforeEach({
+    	service: (connection) => apiGw.start(httpPort, connection.natsUrl), 
+    	bus: bus    	
+    });
+
+    beforeAll(() => {
+    	conf.interceptors = interceptorConfig({
+    		INTERCEPTOR_1: "1;http.*,!http.post.auth;interceptor-1",
+    		INTERCEPTOR_2: "2;*;interceptor-2"
+		});
+    });
+
+    afterAll(() => {
+    	conf.interceptors = []; 
+    });
+
+    it("should invoke interceptor", function (done) {
+    	
+    	testUtils.mockService({
+    		subject: "interceptor-1",
+    		resp: (resp) => {
+    			resp.interceptor1 = true;    			
+				resp.data.wasHere = "interceptor-1";
+    			return resp;			
+    		}
+    	});
+
+    	testUtils.mockService({
+    		subject: "interceptor-2",
+    		resp: (resp) => {    			
+				resp.interceptor2 = true;
+				resp.data.wasHere = "interceptor-2";
+				return resp;
+    		}
+    	});
+
+    	testUtils.mockService({
+    		subject: "http.get.foo",
+    		expectRequest: (req) => {    			
+    			expect(req.reqId).toBeDefined();
+    			expect(req.data.wasHere).toBe("interceptor-2");
+    			expect(req.interceptor1).toBeTruthy();   
+    			expect(req.interceptor2).toBeTruthy();   
+    		}
+    	});
+        
+        get("/foo", function (error, response, body) {        
+            expect(response.statusCode).toBe(200);
+            expect(body.status).toBe(200);
+            done();
+        });
+    });
+
+    it("should return error from interceptor", function (done) {    	   
+    	testUtils.mockService({
+    		subject: "interceptor-1",    		
+    		resp: (resp) => {
+    			resp.interceptor1 = true;				
+    			return resp;			
+    		}
+    	});
+
+    	testUtils.mockService({
+    		subject: "interceptor-2",    		
+    		resp: {
+    			status: 400,
+    			error: {
+    				code: "BAD_REQUEST"
+    			}
+    		}
+    	});
+    	        
+        get("/foo", function (error, response, body) {        
+            expect(response.statusCode).toBe(400);
+            expect(body.status).toBe(400);
+            done();
+        });
+    });
+
+    it("should respond directly from interceptor", function (done) {           
+        testUtils.mockService({
+            subject: "interceptor-1",           
+            resp: {
+                status: 200,
+                interceptAction: "respond",
+                data: {}                
+            }
+        });
+                        
+        get("/foo", function (error, response, body) {        
+            expect(response.statusCode).toBe(200);
+            done();
+        });
+    });
+
+    function get(path, headers, cb) {
+        if (typeof (headers) === "function") {
+            cb = headers;
+        }
+        doRequest("GET", path, headers, true, cb);
+    }
+
+    function doRequest(method, path, headers, json, cb) {
+        request({
+            uri: baseUri + path,
+            method: method,
+            headers: headers,
+            json: json ||  true
+        }, cb);
+    }
+
+});
