99d0a2e170dd7e38db84d0f8d231037d084b48e2
Viktor Söderström <viktor@frostdigital.se>
2017-12-01 13:42:16 +0100

    [FEATURE] Public user websocket connection  (#42)
    * progress
    
    * removes permissions for connecting to bus as a registered user and adds config to not allow public users to connect

--- spec/FrusterWebBus.spec.js
+++ spec/FrusterWebBus.spec.js
@@ -12,10 +12,12 @@ const express = require("express");
 const WebSocket = require("ws");
 const FrusterWebBus = require("../lib/web-bus/FrusterWebBus");
 const testUtils = require("fruster-test-utils");
+const constants = require("../lib/constants");
 
 
 describe("FrusterWebBus", () => {
     const wsEndpointSubject = `ws.post.hello.:userId`;
+    const wsEndpointSubjectMustBeLoggedIn = `ws.post.hello.locked.:userId`;
     const mockUserId = "hello-there-id";
     const mockUserId2 = "hello-there-id-1337";
     let natsServer;
@@ -33,24 +35,51 @@ describe("FrusterWebBus", () => {
 
             server = await apiGw.start(httpPort, connection.natsUrl);
 
-            bus.subscribe(wsEndpointSubject, (req) => {
-                if (req.data.shouldFail) {
+            bus.subscribe({
+                subject: wsEndpointSubject,
+                handle: (req) => {
+                    if (req.data && req.data.shouldFail) {
+                        return {
+                            status: 500,
+                            error: {
+                                code: "INTERNAL_SERVER_ERROR",
+                                title: "internal server error",
+                                detail: "Fail because it had to fail"
+                            }
+                        };
+                    }
                     return {
-                        status: 500,
-                        error: {
-                            code: "INTERNAL_SERVER_ERROR",
-                            title: "internal server error",
-                            detail: "Fail because it had to fail"
+                        status: 200,
+                        data: {
+                            hello: "hello " + req.params.userId
                         }
                     };
                 }
-                return {
-                    status: 200,
-                    data: {
-                        hello: "hello " + req.params.userId
-                    }
-                };
             });
+
+            bus.subscribe({
+                subject: wsEndpointSubjectMustBeLoggedIn,
+                mustBeLoggedIn: true,
+                handle: (req) => {
+                    if (req.data && req.data.shouldFail) {
+                        return {
+                            status: 500,
+                            error: {
+                                code: "INTERNAL_SERVER_ERROR",
+                                title: "internal server error",
+                                detail: "Fail because it had to fail"
+                            }
+                        };
+                    }
+                    return {
+                        status: 200,
+                        data: {
+                            hello: "hello " + req.params.userId
+                        }
+                    };
+                }
+            });
+
         },
         mockNats: true,
         bus: bus,
@@ -59,6 +88,11 @@ describe("FrusterWebBus", () => {
                 test: true
             });
         }
+    });
+
+    afterEach((done) => {
+        conf.allowPublicWebsocketConnections = true;
+        done();
     });
 
     function registerMockAuthServiceResponse() {
@@ -70,13 +104,13 @@ describe("FrusterWebBus", () => {
                         id: mockUserId,
                         firstName: "bob",
                         lastName: "fred",
-                        scopes: conf.webSocketPermissionScope
+                        scopes: ["profile.get"]
                     },
                     hello2: {
                         id: mockUserId2,
                         firstName: "bob",
                         lastName: "fred",
-                        scopes: conf.webSocketPermissionScope
+                        scopes: ["profile.get"]
                     }
                 };
 
@@ -110,12 +144,78 @@ describe("FrusterWebBus", () => {
             done();
         });
 
-        ws.on("close", () => {
-            done.fail("websocket closed");
+        ws.on("close", (code, reason) => {
+            done.fail(`websocket closed: ${code} ${reason}`);
         });
 
         setTimeout(() => {
             bus.request("ws.out.hello-there-id.hello", messageToSend);
+        }, 100);
+    });
+
+    it("should be possible to connect to web bus as public user", done => {
+        const messageToSend = {
+            reqId: uuid.v4(),
+            data: { some: "data" }
+        };
+
+        registerMockAuthServiceResponse();
+
+        const ws = new WebSocket(webSocketBaseUri, [], {});
+
+        ws.on("message", json => {
+            const message = JSON.parse(json.toString());
+            expect(message.data.hello).toBe("hello BOB", "json.data.hello");
+
+            done();
+        });
+
+        ws.on("close", (code, reason) => {
+            done.fail(`websocket closed: ${code} ${reason}`);
+        });
+
+        setTimeout(() => {
+            ws.send(new Buffer(JSON.stringify({
+                subject: wsEndpointSubject.replace(":userId", "BOB"),
+                message: {
+                    reqId: "hello",
+                    transactionId: "transactionId"
+                }
+            })));
+        }, 100);
+    });
+
+    it("should not be possible to make requests to endpoints that require user to be logged in as public user connected to websocket", done => {
+        const messageToSend = {
+            reqId: uuid.v4(),
+            data: { some: "data" }
+        };
+
+        registerMockAuthServiceResponse();
+
+        const ws = new WebSocket(webSocketBaseUri, [], {});
+
+        ws.on("message", json => {
+            const message = JSON.parse(json.toString());
+
+            expect(message.status).toBe(403, "message.status");
+            expect(message.error.code).toBe("PERMISSION_DENIED", "message.error.code");
+
+            done();
+        });
+
+        ws.on("close", (code, reason) => {
+            done.fail(`websocket closed: ${code} ${reason}`);
+        });
+
+        setTimeout(() => {
+            ws.send(new Buffer(JSON.stringify({
+                subject: wsEndpointSubjectMustBeLoggedIn.replace(":userId", "BOB"),
+                message: {
+                    reqId: "hello",
+                    transactionId: "transactionId"
+                }
+            })));
         }, 100);
     });
 
@@ -141,8 +241,8 @@ describe("FrusterWebBus", () => {
             done();
         });
 
-        ws.on("close", () => {
-            done.fail("websocket closed");
+        ws.on("close", (code, reason) => {
+            done.fail(`websocket closed: ${code} ${reason}`);
         });
 
         setTimeout(() => {
@@ -181,8 +281,8 @@ describe("FrusterWebBus", () => {
             done();
         });
 
-        ws.on("close", () => {
-            done.fail("websocket closed");
+        ws.on("close", (code, reason) => {
+            done.fail(`websocket closed: ${code} ${reason}`);
         });
 
         setTimeout(() => {
@@ -207,8 +307,8 @@ describe("FrusterWebBus", () => {
 
         const ws = new WebSocket(webSocketBaseUri, [], { headers: { cookie: "jwt=hello" } });
         const ws2 = new WebSocket(webSocketBaseUri, [], { headers: { cookie: "jwt=hello2" } });
-        ws.on("close", () => { done.fail("websocket closed"); });
-        ws2.on("close", () => { done.fail("websocket2 closed"); });
+        ws.on("close", (code, reason) => { done.fail(`websocket closed: ${code} ${reason}`); });
+        ws2.on("close", (code, reason) => { done.fail(`websocket2 closed: ${code} ${reason}`); });
 
         ws.on("message", json => { wsGotMessage = true; });
         ws2.on("message", json => {
@@ -225,38 +325,6 @@ describe("FrusterWebBus", () => {
         }, 100);
     });
 
-    it("should not allow users without the correct scopes to connect", done => {
-        bus.subscribe({
-            subject: "auth-service.decode-token",
-            responseSchema: "",
-            handle: (req) => {
-                return {
-                    status: 200,
-                    data: {
-                        id: "hello-there-id",
-                        scopes: ["read.a.book"]
-                    }
-                };
-            }
-        });
-
-        const ws = new WebSocket(webSocketBaseUri, [], {
-            headers: { cookie: "jwt=hello" }
-        });
-
-        ws.on("close", () => {
-            done();
-        });
-    });
-
-    it("should not allow non-logged in users to connect", done => {
-        const ws = new WebSocket(webSocketBaseUri);
-
-        ws.on("close", () => {
-            done();
-        });
-    });
-
     it("should allow broadcasts", done => {
         const message = {
             reqId: uuid.v4(),
@@ -271,8 +339,50 @@ describe("FrusterWebBus", () => {
         const ws = new WebSocket(webSocketBaseUri, [], { headers: { cookie: "jwt=hello" } });
         const ws2 = new WebSocket(webSocketBaseUri, [], { headers: { cookie: "jwt=hello" } });
 
-        ws.on("close", () => { done.fail("websocket closed"); });
-        ws2.on("close", () => { done.fail("websocket2 closed"); });
+        ws.on("close", (code, reason) => { done.fail(`websocket closed: ${code} ${reason}`); });
+        ws2.on("close", (code, reason) => { done.fail(`websocket2 closed: ${code} ${reason}`); });
+
+        ws.on("message", (json) => {
+            const message = JSON.parse(json.toString());
+
+            expect(message.subject).toBe("ws.out.hello-there-id.new-message");
+            expect(message.reqId).toBe(message.reqId);
+            expect(message.data.some).toBe(message.data.some);
+            expect(message.subject).toBe(message.subject);
+
+            wsGotMessage = true;
+        });
+
+        ws2.on("message", (json) => {
+            ws2GotMessage = true;
+
+            if (wsGotMessage && ws2GotMessage)
+                done();
+            else
+                done.fail();
+        });
+
+        setTimeout(() => {
+            bus.request("ws.out.*.new-message", message);
+        }, 100);
+    });
+
+    it("should allow broadcasts to public users", done => {
+        const message = {
+            reqId: uuid.v4(),
+            data: { some: "data" }
+        };
+
+        let wsGotMessage = false;
+        let ws2GotMessage = false;
+
+        registerMockAuthServiceResponse();
+
+        const ws = new WebSocket(webSocketBaseUri, [], { headers: { cookie: "jwt=hello" } });
+        const ws2 = new WebSocket(webSocketBaseUri, [], {});
+
+        ws.on("close", (code, reason) => { done.fail(`websocket closed: ${code} ${reason}`); });
+        ws2.on("close", (code, reason) => { done.fail(`websocket2 closed: ${code} ${reason}`); });
 
         ws.on("message", (json) => {
             const message = JSON.parse(json.toString());
@@ -310,8 +420,8 @@ describe("FrusterWebBus", () => {
             headers: { cookie: "jwt=hello" }
         });
 
-        ws.on("close", () => {
-            done.fail("websocket closed");
+        ws.on("close", (code, reason) => {
+            done.fail(`websocket closed: ${code} ${reason}`);
         });
 
         ws.on("message", (json) => {
@@ -321,14 +431,14 @@ describe("FrusterWebBus", () => {
             expect(response.data).toBeDefined("response.data");
             expect(response.data.hello).toBe("hello BOB", "response.data.hello");
             expect(response.reqId).toBe(reqId, "response.reqId");
-            expect(response.subject).toBe(`res.${transactionId}.${wsEndpointSubject.replace(":userId", userId)}`);
+            expect(response.subject).toBe(`res.${transactionId}.${wsEndpointSubjectMustBeLoggedIn.replace(":userId", userId)}`);
 
             done();
         });
 
         setTimeout(() => {
             ws.send(new Buffer(JSON.stringify({
-                subject: wsEndpointSubject.replace(":userId", userId),
+                subject: wsEndpointSubjectMustBeLoggedIn.replace(":userId", userId),
                 message: {
                     reqId: reqId,
                     transactionId: transactionId,
@@ -355,9 +465,8 @@ describe("FrusterWebBus", () => {
             headers: { cookie: "jwt=hello" }
         });
 
-        ws.on("close", (a, b, c, d) => {
-            console.log(a, b, c, d);
-            done.fail("websocket closed");
+        ws.on("close", (code, reason) => {
+            done.fail(`websocket closed: ${code} ${reason}`);
         });
 
         ws.on("message", (json) => {
@@ -372,7 +481,7 @@ describe("FrusterWebBus", () => {
 
         setTimeout(() => {
             ws.send(new Buffer(JSON.stringify({
-                subject: wsEndpointSubject.replace(":userId", userId),
+                subject: wsEndpointSubjectMustBeLoggedIn.replace(":userId", userId),
                 message: {
                     reqId: reqId,
                     transactionId: transactionId,
@@ -398,8 +507,8 @@ describe("FrusterWebBus", () => {
             headers: { cookie: "jwt=hello" }
         });
 
-        ws.on("close", () => {
-            done.fail("websocket closed");
+        ws.on("close", (code, reason) => {
+            done.fail(`websocket closed: ${code} ${reason}`);
         });
 
         ws.on("message", (json) => {
@@ -431,7 +540,8 @@ describe("FrusterWebBus", () => {
             headers: { cookie: "jwt=hello" }
         });
 
-        ws.on("close", () => {
+        ws.on("close", (code, reason) => {
+            expect(reason).toBe(constants.websocketErrorCodes.USER_DISCONNECTED, "reason");
             done();
         });
 
@@ -453,7 +563,8 @@ describe("FrusterWebBus", () => {
             headers: { cookie: "jwt=hello" }
         });
 
-        ws.on("close", () => {
+        ws.on("close", (code, reason) => {
+            expect(reason).toBe(constants.websocketErrorCodes.USER_DISCONNECTED, "reason");
             done();
         });
 
@@ -464,6 +575,23 @@ describe("FrusterWebBus", () => {
             });
         });
 
+    });
+
+    it("should not be possible to connect to websocket as public user if allowPublicWebsocketConnections is set to false", async done => {
+        conf.allowPublicWebsocketConnections = false;
+        const reqId = uuid.v4();
+
+        registerMockAuthServiceResponse();
+
+        const ws = new WebSocket(webSocketBaseUri, [], {
+            headers: {}
+        });
+
+        ws.on("close", (code, reason) => {
+            conf.allowPublicWebsocketConnections = true;
+            expect(reason).toBe(constants.websocketErrorCodes.PERMISSION_DENIED, "reason");
+            done();
+        });
     });
 
     it("should return ok if client could not be found", async done => {
